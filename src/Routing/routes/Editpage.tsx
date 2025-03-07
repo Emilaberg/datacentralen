0301b0ff10842d738f79678060f0429aaf856d4f
Emil Åberg <emil.aberg@live.com>
2025-03-07 21:59:34 +0100

    file upload, support html, md. choose a file and upload. save to localstorage just for demo

--- src/Routing/routes/Editpage.tsx
+++ src/Routing/routes/Editpage.tsx
@@ -1,10 +1,46 @@
 import { useContext, useState } from "react";
-import SlateEditor from "../../components/SlateEditor/SlateEditor";
-import { Editable } from "slate-react";
-
+import DOMPurify from "dompurify";
+import { marked } from "marked";
 const Editpage = () => {
+  const [previewText, setPreviewText] = useState(
+    JSON.parse(localStorage.getItem("savedString")) ||
+      "din uppladdade fil syns här..."
+  );
+
+  const handleFileUpload = async (event: object) => {
+    const uploadedFile = event.target.files.item(0);
+
+    console.log(uploadedFile);
+    const splitString = uploadedFile.name.split(".");
+    const documentType = splitString[splitString.length - 1];
+
+    let textContent = await uploadedFile.text();
+
+    if (documentType === "md") {
+      textContent = marked(textContent);
+    }
+
+    console.log(typeof textContent);
+    console.log(marked(textContent));
+
+    setPreviewText(textContent);
+
+    console.log(textContent);
+  };
+
+  const handlePublish = (event: object) => {
+    localStorage.setItem("savedString", JSON.stringify(previewText));
+  };
+
+  const handleDelete = () => {
+    const fileUploadBtn = document.querySelector("#uploadFile");
+
+    fileUploadBtn.value = null;
+    localStorage.removeItem("savedString");
+    setPreviewText("");
+  };
   return (
-    <section className="h-screen flex flex-col items-center">
+    <section className="min-h-screen flex flex-col items-center">
       <article className="w-1/2 my-10">
         <span className="text-[#777777] text-lg font-light">
           datastrukturer
@@ -27,8 +63,42 @@ const Editpage = () => {
 
       <hr className="border-2 w-1/2 border-[#777777] my-10" />
 
-      <article className="w-1/2 h-fit mx-auto border-dashed border-1 border-black">
-        <SlateEditor />
+      <div className="my-2">
+        <h1>ladda upp en fil</h1>
+        <input
+          className="skibiditoilet bg-gray-200 px-2 border-solid border border-black my-2"
+          // onInput={(e) => console.log(e.target.files)}
+          onInput={(e) => handleFileUpload(e)}
+          type="file"
+          name=""
+          id="uploadFile"
+          accept=".md, .html"
+        />
+
+        <button
+          className={`${
+            previewText.length > 100
+              ? "bg-green-500 cursor-pointer block"
+              : "hidden"
+          } text-white font-semibold mx-auto px-4 py-2 rounded-xl`}
+          type="button"
+          onClick={(e) => handlePublish(e)}
+        >
+          Save and publish
+        </button>
+        <button
+          className={`bg-red-400 cursor-pointer block text-white font-semibold mx-auto px-4 py-2 rounded-xl`}
+          onClick={() => handleDelete()}
+        >
+          Delete from saved
+        </button>
+      </div>
+
+      <article className="w-1/2 mx-auto border-dashed border-1 border-black">
+        {/* <SlateEditor /> */}
+        <div
+          dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(previewText) }}
+        ></div>
       </article>
     </section>
   );
--- src/components/SlateEditor/SlateEditor.tsx
+++ src/components/SlateEditor/SlateEditor.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 import { createEditor, Transforms, Element, Editor } from "slate";
 import { Slate, Editable, withReact } from "slate-react";
 
@@ -16,148 +16,45 @@ declare module "slate" {
   }
 }
 
-const initialValue: Descendant[] = [
-  {
-    type: "paragraph",
-    children: [{ text: "A line of text in a paragraph." }],
-  },
-];
-// HELPER FUNCTION
-// Define our own custom set of helpers.
-const CustomEditor = {
-  isBoldMarkActive(editor: any) {
-    const marks = Editor.marks(editor);
-    return marks ? marks.bold === true : false;
-  },
-
-  isCodeBlockActive(editor: any) {
-    const [match] = Editor.nodes(editor, {
-      match: (n) => n.type === "code",
-    });
-
-    return !!match;
-  },
-
-  toggleBoldMark(editor: any) {
-    const isActive = CustomEditor.isBoldMarkActive(editor);
-    if (isActive) {
-      Editor.removeMark(editor, "bold");
-    } else {
-      Editor.addMark(editor, "bold", true);
-    }
-  },
-
-  toggleCodeBlock(editor: any) {
-    const isActive = CustomEditor.isCodeBlockActive(editor);
-    Transforms.setNodes(
-      editor,
-      { type: isActive ? null : "code" },
-      { match: (n) => Editor.isBlock(editor, n) }
-    );
-  },
-};
+// const initialValue: Descendant[] = [
+//   {
+//     type: "paragraph",
+//     children: [{ text: "A line of text in a paragraph." }],
+//   },
+// ];
 
 const SlateEditor = () => {
-  // Create a Slate editor object that won't change across renders.
   const [editor] = useState(() => withReact(createEditor()));
-
-  // Define a rendering function based on the element passed to `props`. We use
-  // `useCallback` here to memoize the function for subsequent renders.
-  const renderElement = useCallback((props: any) => {
-    switch (props.element.type) {
-      case "code":
-        return <CodeElement {...props} />;
-      default:
-        return <DefaultElement {...props} />;
-    }
-  }, []);
-
-  //FOR BOLD TEXT
-  //https://docs.slatejs.org/walkthroughs/04-applying-custom-formatting#:~:text=And%20now%2C%20let%27s%20tell%20Slate%20about%20that%20leaf.%20To%20do%20that%2C%20we%27ll%20pass%20in%20the%20renderLeaf%20prop%20to%20our%20editor.
-
-  // Define a leaf rendering function that is memoized with `useCallback`.
-  const renderLeaf = useCallback((props) => {
-    return <Leaf {...props} />;
-  }, []);
+  // Update the initial content to be pulled from Local Storage if it exists.
+  const initialValue: Descendant[] = useMemo(
+    () =>
+      JSON.parse(localStorage.getItem("content")) || [
+        {
+          type: "paragraph",
+          children: [{ text: "A line of text in a paragraph." }],
+        },
+      ],
+    []
+  );
 
   return (
     <Slate
       editor={editor}
       initialValue={initialValue}
+      onChange={(value) => {
+        const isAstChange = editor.operations.some(
+          (op) => "set_selection" !== op.type
+        );
+        if (isAstChange) {
+          // Save the value to Local Storage.
+          const content = JSON.stringify(value);
+          localStorage.setItem("content", content);
+        }
+      }}
     >
-      <div className="flex gap-2 ">
-        <button
-          className="cursor-pointer hover:bg-gray-100"
-          onMouseDown={(event) => {
-            event.preventDefault();
-            CustomEditor.toggleBoldMark(editor);
-          }}
-        >
-          Bold
-        </button>
-        <button
-          className="cursor-pointer hover:bg-gray-100"
-          onMouseDown={(event) => {
-            event.preventDefault();
-            CustomEditor.toggleCodeBlock(editor);
-          }}
-        >
-          Code Block
-        </button>
-      </div>
-      <Editable
-        // Pass in the `renderElement` function.
-        renderElement={renderElement}
-        // Pass in the `renderLeaf` function.
-        renderLeaf={renderLeaf}
-        onKeyDown={(event) => {
-          if (!event.ctrlKey) {
-            return;
-          }
-
-          // Replace the `onKeyDown` logic with our new commands.
-          switch (event.key) {
-            case "'": {
-              event.preventDefault();
-              CustomEditor.toggleCodeBlock(editor);
-              break;
-            }
-
-            case "b": {
-              event.preventDefault();
-              CustomEditor.toggleBoldMark(editor);
-              break;
-            }
-          }
-        }}
-      />
+      <Editable />
     </Slate>
   );
 };
 
 export default SlateEditor;
-
-// Define a React component renderer for our code blocks.
-const CodeElement = (props: any) => {
-  return (
-    <pre {...props.attributes}>
-      <code>{props.children}</code>
-    </pre>
-  );
-};
-
-const DefaultElement = (props: any) => {
-  return <p {...props.attributes}>{props.children}</p>;
-};
-
-// Define a React component to render leaves with bold text.
-const Leaf = (props: any) => {
-  return (
-    <span
-      {...props.attributes}
-      style={{ fontWeight: props.leaf.bold ? "bold" : "normal" }}
-    >
-      {props.children}
-    </span>
-  );
-};
