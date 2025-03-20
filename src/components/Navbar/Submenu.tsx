d9d340b1b37a908d4b011d7a2a67f5d9411c9b71
emilaberg <emil.aberg@live.com>
2025-03-20 16:59:25 +0100

    thiiiiiick border

--- src/components/Navbar/Submenu.tsx
+++ src/components/Navbar/Submenu.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Submenu = () => {
   return (
     <div className="absolute w-full left-0 top-0 hidden group-hover:block">
-      <ul className=" w-[200%] mt-10 hidden group-hover:flex flex-col gap-2 bg-white px-6 capitalize rounded-2xl border border-[#96C9E3] py-4">
+      <ul className=" w-[200%] mt-10 hidden group-hover:flex flex-col gap-2 bg-white px-6 capitalize rounded-2xl border-2 border-[#96C9E3] py-4">
         <li>
           <Link to="#" className="font-medium text-lg">BubbleSort</Link>
           <p className="text-black/50 text-sm font-roboto">Jämför och byter intilliggande element tills listan är sorterad</p>
