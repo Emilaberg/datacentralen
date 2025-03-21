0da65f7ce158e1206992442f1ffdfe64d054fbe1
emilaberg <emil.aberg@live.com>
2025-03-21 15:11:19 +0100

    changed routing so we can have multiple layouts

--- src/Routing/Router.tsx
+++ src/Routing/Router.tsx
@@ -3,29 +3,28 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Index from "./routes/Index";
 import TestaAlgoritm from "./routes/TestaAlgoritm";
 import Laroportal from "./routes/Laroportal";
-import AuthProvider from "../Auth/AuthProvider";
 import AboutUs from "./routes/About us/AboutUs";
 import Layout from "./Layout/Layout";
+import Loginpage from "./routes/Login page/Loginpage";
 
 function Router() {
   return (
     <BrowserRouter>
-      <AuthProvider>
-        {/* <Navbar/> */}
-        <Layout>
-          <Routes >
-              <Route index element={<Index/>}/>
-              <Route path='laroportal' element={<Laroportal/>}/>
-              <Route path="/laroportal/:id" element={<div>specific id</div>}/> 
-              <Route path="/testa-algoritm" element={<TestaAlgoritm />} />
-              <Route path='/about-us' element={<AboutUs/>}/>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+            <Route index element={<Index/>}/>
+            <Route path='laroportal' element={<Laroportal/>}/>
+            <Route path="/laroportal/:id" element={<div>specific id</div>}/> 
+            <Route path="/testa-algoritm" element={<TestaAlgoritm />} />
+            <Route path='/about-us' element={<AboutUs/>}/>
 
-              <Route path='*' element={<div>error 404</div>}/>
-          </Routes>
-        </Layout>
+            <Route path='*' element={<div>error 404</div>}/>
+        </Route>            
         
-        {/* <Footer/> */}
-      </AuthProvider>
+        <Route path="/login" element={<Loginpage/>}/>
+        <Route path="*" element={<div>Error 404 page not found</div>} />
+      
+      </Routes>    
     </BrowserRouter>
   );
 }
