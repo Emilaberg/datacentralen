d5b31331775e16de719b3210b87fde1d2c0eb2c4
Johannes Nilsson <e.johannesnilsson@gmail.com>
2025-04-14 13:36:33 +0200

    about us taking some form

--- src/Routing/routes/About us/AboutUs.tsx
+++ src/Routing/routes/About us/AboutUs.tsx
@@ -1,20 +1,35 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import ApiService from "../../../Services/ApiService";
 import { useQuery } from "@tanstack/react-query";
+import { ArticleProps } from "../../Types/types";
 
-// const AboutUs = () => {
-//   const { Articles, ArticleCardDTO } = ApiService();
-//   const { sortingArticles, sortingArticlesCardDTO } = useState(data.filter((article) => article.category === "sorting"));
+const AboutUs = () => {
+  const { Articles } = ApiService();
+  const [sortingArticles, setSortingArticles] = useState([]);
+  const [dataStructureArticles, setDataStructureArticles] = useState([]);
 
-//   const { data, isLoading } = useQuery({
-//     queryFn: Articles,
-//     queryKey: ["articles"],
-//   });
+  useEffect(() => {
+    if (data) {
+      const filteredSortingArticles = data.filter(
+        (articles: ArticleProps) => articles.type === "Sorteringsalgoritm"
+      );
+      const filteredDataStructureArticles = data.filter(
+        (articles: ArticleProps) => articles.type === "Datastruktur"
+      );
+      setSortingArticles(filteredSortingArticles);
+      setDataStructureArticles(filteredDataStructureArticles);
+    }
+  });
+
+  const { data, isLoading } = useQuery({
+    queryFn: Articles,
+    queryKey: ["articles"],
+  });
 
   console.log("data", data);
   return (
-    <section className=" bg-blanchOrange h-full w-full mr-15 ml-15 flex flex-col justify-center items-center mt-20">
-      <article className=" w-5/6 h-[650px] flex bg-[#CF6227]/5 p-10 radious-xl rounded-4xl">
+    <section className=" overflow-hidden bg-blanchOrange h-full w-full  flex flex-col justify-center items-center mt-20">
+      <article className="mr-15 ml-15 w-5/6 h-[650px] flex bg-[#CF6227]/10 p-10 radious-xl rounded-4xl">
         <div className="flex gap-20 items-center">
           <div className="relative h-4/6 w-3/6">
             <img
@@ -35,7 +50,7 @@ import { useQuery } from "@tanstack/react-query";
           </div>
         </div>
       </article>
-      <article className="mt-10 w-5/6 h-[650px] flex bg-[#D7875D]/10 p-10 radious-xl rounded-4xl">
+      <article className="mr-15 ml-15 mt-10 w-5/6 h-[650px] flex bg-[#D7875D]/15 p-10 radious-xl rounded-4xl">
         <div className="flex gap-20 items-center">
           <div className="flex flex-col w-2/4 pb-15 gap-5">
             <h1 className="text-[50px]">
@@ -54,6 +69,24 @@ import { useQuery } from "@tanstack/react-query";
               className="h-full w-full object-cover rounded-2xl"
             />
             <div className="absolute top-0 left-0 h-full w-full bg-white/40 rounded-lg"></div>
+          </div>
+        </div>
+      </article>
+      <article className="mr-15 ml-15 mt-10 w-5/6 h-[650px] flex bg-[#D7875D]/15 p-10 radious-xl rounded-4xl">
+        <div className="flex justify-evenly items-center h-full w-full">
+          <div className="flex p-10 flex-col justify-center items-center gap-2 rounded-full bg-[#D7875D]/10 h-[350px] w-[350px]">
+            <h1 className="text-[50px] font-bold">{sortingArticles.length}</h1>
+            <p className="text-[17px] italic text-center">
+              Interaktiva sorteringsalgoritmer
+            </p>
+          </div>
+          <div className="p-10 flex flex-col justify-center items-center gap-2 rounded-full bg-[#D7875D]/10 h-[350px] w-[350px]">
+            <h1 className="text-[50px] font-bold">
+              {dataStructureArticles.length}
+            </h1>
+            <p className="text-[17px] italic text-center">
+              Spännande datastrukturer att läsa om
+            </p>
           </div>
         </div>
       </article>
